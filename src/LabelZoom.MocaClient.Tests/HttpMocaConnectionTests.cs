b88aa737c81ebb711b8fc2e522842338a64aa131
Robert Faust <robert.faust@labelzoom.net>
2024-01-14 22:12:43 -0600

    Code cleanup

--- src/LabelZoom.MocaClient.Tests/HttpMocaConnectionTests.cs
+++ src/LabelZoom.MocaClient.Tests/HttpMocaConnectionTests.cs
@@ -39,7 +39,7 @@ public class HttpMocaConnectionTests
     [Fact]
     public async Task WithValidLogin_TestPublishDataWithMultipleRow()
     {
-        using (MocaConnection conn = new HttpMocaConnection(url))
+        using (HttpMocaConnection conn = new HttpMocaConnection(url))
         {
             await conn.Login(user, password);
             MocaResponse res = await conn.Execute("publish data where a = 1 and b = 2 & publish data where a = 3 and b = 4");
